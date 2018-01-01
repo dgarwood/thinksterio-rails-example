ee7b3b41bf0127bfb3211319237a91b737105cdb
Dewey Garwood <dewey.garwood@gmail.com>
2018-01-01 16:19:13 -0700

    configure devise for json requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  scope :api, defaults: { format: :json } do
+    devise_for :users
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
