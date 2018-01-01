60741fe8b7977b8519a6beabd44928f1657d5688
Dewey Garwood <dewey.garwood@gmail.com>
2018-01-01 15:39:55 -0700

    configure as json API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  before_action :underscore_params!
+  respond_to :json
+
+  private
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
 end
